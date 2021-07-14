3794e90451468a2a7573486f650dd166562adbea
nate smith <vilmibm@github.com>
2021-07-14 12:45:52 -0500

    add cobra

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vilmibm/gh-user-status
 
 go 1.16
+
+require github.com/spf13/cobra v1.2.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "user-status",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("TODO")
+	},
+}
 
 func main() {
-	fmt.Printf("TODO")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
