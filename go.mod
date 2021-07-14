2d1e3d7c210a2e623b4803214ee84c93e3ef9852
nate smith <vilmibm@github.com>
2021-07-14 17:37:41 -0500

    broken wip

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/vilmibm/gh-user-status
 
 go 1.16
 
-require github.com/spf13/cobra v1.2.1 // indirect
+require (
+	github.com/cli/safeexec v1.0.0 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"time"
 
+	"github.com/cli/safeexec"
 	"github.com/spf13/cobra"
 )
 
@@ -25,7 +27,7 @@ type setOptions struct {
 func setCmd() *cobra.Command {
 	opts := setOptions{}
 	cmd := &cobra.Command{
-		Use:   "set [<status>]",
+		Use:   "set <status>",
 		Short: "set your GitHub status",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -46,21 +48,93 @@ func runSet(opts setOptions) error {
 	// TODO expiry flag
 	// TODO emoji flag
 	// TODO org flag
+	/*
+			# We'll get you started with a simple query showing your username!
+		mutation ($status: ChangeUserStatusInput!) {
+		    changeUserStatus(input: $status) {
+		      status {
+		        emoji
+		        expiresAt
+		        limitedAvailability: indicatesLimitedAvailability
+		        message
+		      }
+		    }
+		  }*/
 	fmt.Printf("set %s\n", opts.Status)
 	return nil
+}
+
+type getOptions struct {
+	Login string
 }
 
 func getCmd() *cobra.Command {
 	// TODO get arbitrary user
 	// TODO get current user
 	return &cobra.Command{
-		Use:   "get",
-		Short: "get a GitHub user's status",
+		Use:   "get [<username>]",
+		Short: "get a GitHub user's status or your own",
+		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("TODO gc")
-			return nil
+			opts := getOptions{}
+			if len(args) > 0 {
+				opts.Login = args[0]
+			}
+			return runGet(opts)
 		},
 	}
+}
+
+type status struct {
+	IndicatesLimitedAvailability bool
+	Message                      string
+	Emoji                        string
+}
+
+func runGet(opts getOptions) error {
+	if opts.Login == "" {
+		panic("TODO")
+	}
+	s, err := apiStatus(opts.Login)
+	if err != nil {
+		return err
+	}
+
+	// TODO
+	fmt.Println(s)
+
+	return nil
+}
+
+func apiStatus(login string) (status, error) {
+	query := fmt.Sprintf(`query getUserStatus {
+		user(login:"%s") {
+			status {
+				indicatesLimitedAvailability
+				message
+				emoji
+		}}}`, login)
+
+	// TODO
+	fmt.Println(query)
+	ghBin, err := safeexec.LookPath("gh")
+	if err != nil {
+		return status{}, err
+	}
+	// TODO gh api just opaquely returning exit status 1, why? is there an
+	// escaping problem? gh api is running fine manually.
+	// the env thing is stupid and didn't help
+	queryValue := fmt.Sprintf("STATUS_QUERY=query='%s'", query)
+	cmd := exec.Command(ghBin, "api", "graphql", "-f", "$STATUS_QUERY")
+	cmd.Env = append(os.Environ(), queryValue)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("DBG %#v\n", cmd)
+		fmt.Printf("DBG %#v\n", err)
+		return status{}, err
+	}
+
+	return status{}, nil
 }
 
 func main() {
