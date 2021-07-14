2c632291830a5d9838036fc73ad920f5644d8ecd
nate smith <vilmibm@github.com>
2021-07-14 12:59:49 -0500

    get and set stubs

--- main.go
+++ main.go
@@ -7,15 +7,42 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use: "user-status",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("TODO")
-	},
+func rootCmd() *cobra.Command {
+	return &cobra.Command{
+		Use: "user-status",
+	}
+}
+
+func setCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "set",
+		Short: "set your GitHub status",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("TODO sc")
+			return nil
+		},
+	}
+}
+
+func getCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "get",
+		Short: "get a GitHub user's status",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("TODO gc")
+			return nil
+		},
+	}
 }
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	rc := rootCmd()
+	sc := setCmd()
+	gc := getCmd()
+	rc.AddCommand(sc)
+	rc.AddCommand(gc)
+
+	if err := rc.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
